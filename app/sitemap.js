325c171141325be5037ff893604cc8f091a42650
Md. Mohsin Kabir <mohsinkabirseo@gmail.com>
2026-07-17 22:10:38 +0600

    latest

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,8 +1,82 @@
 export default function sitemap() {
   return [
     {
-      url: 'https://alnawrasplus.com',
-      lastModified: new Date(),
+      url: "https://alnawrasplus.com",
+      lastModified,
+    },
+
+    {
+      url: "https://alnawrasplus.com/services",
+      lastModified,
+    },
+    {
+      url: "https://alnawrasplus.com/services/house-cleaning-kuwait",
+      lastModified,
+    },
+    {
+      url: "https://alnawrasplus.com/services/apartment-cleaning-kuwait",
+      lastModified,
+    },
+    {
+      url: "https://alnawrasplus.com/services/office-cleaning-kuwait",
+      lastModified,
+    },
+    {
+      url: "https://alnawrasplus.com/services/sofa-cleaning-kuwait",
+      lastModified,
+    },
+    {
+      url: "https://alnawrasplus.com/services/deep-cleaning-kuwait",
+      lastModified,
+    },
+    {
+      url: "https://alnawrasplus.com/services/floor-cleaning-kuwait",
+      lastModified,
+    },
+    {
+      url: "https://alnawrasplus.com/services/carpet-cleaning-kuwait",
+      lastModified,
+    },
+    {
+      url: "https://alnawrasplus.com/services/bathroom-cleaning-kuwait",
+      lastModified,
+    },
+    {
+      url: "https://alnawrasplus.com/services/kitchen-cleaning-kuwait",
+      lastModified,
+    },
+    {
+      url: "https://alnawrasplus.com/services/window-cleaning-kuwait",
+      lastModified,
+    },
+    {
+      url: "https://alnawrasplus.com/services/mattress-cleaning-kuwait",
+      lastModified,
+    },
+    {
+      url: "https://alnawrasplus.com/services/chandelier-cleaning-kuwait",
+      lastModified,
+    },
+    {
+      url: "https://alnawrasplus.com/services/janitorial-services-kuwait",
+      lastModified,
+    },
+    {
+      url: "https://alnawrasplus.com/services/move-in-move-out-cleaning-kuwait",
+      lastModified,
+    },
+
+    {
+      url: "https://alnawrasplus.com/about",
+      lastModified,
+    },
+    {
+      url: "https://alnawrasplus.com/contact",
+      lastModified,
+    },
+    {
+      url: "https://alnawrasplus.com/blog",
+      lastModified,
     },
   ];
 }
